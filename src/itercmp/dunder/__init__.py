31d1a284d8a51e2be5f19107bdf0d090fa5cccff
Johannes <johannes.programming@gmail.com>
2026-06-16 13:17:20 +0200

    tested

--- src/itercmp/dunder/__init__.py
+++ src/itercmp/dunder/__init__.py
@@ -5,6 +5,12 @@ from typing import Any
 
 __all__ = ["ge", "gt", "le", "lt", "ordering"]
 
+#def eq(x: Collection[Any], y: Collection[Any], /) -> bool:
+#    for a, b in zip(x, y):
+#        if a is b or a == b:
+#            continue
+#        return False
+#    return len(x) == len(y)
 
 def ordering(
     operator: Callable[[Any, Any], Any],
